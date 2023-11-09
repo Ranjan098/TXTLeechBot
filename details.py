b60da13d718b17710d682840ec2fbb161c13c3c9
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-09 10:21:03 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 12606917
 api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
-bot_token = "6593486018:AAFuLw2UcYz6G8mzEu2kO4c83VkOBDnuFVk"
+bot_token = "6914844052:AAH4dwEH2UWs3aJxCWhHtBvlzv1ODOSuMpY"
 
